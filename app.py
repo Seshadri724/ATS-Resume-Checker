8e3b8d5d4c067800e1c4845d9763e00679545a71
Seshadri724 <158801901+Seshadri724@users.noreply.github.com>
2024-09-04 12:44:24 +0530

    app.py

--- app.py
+++ app.py
@@ -26,12 +26,7 @@ def input_pdf_text(uploaded_file):
 
 input_prompt = """ As an experienced ATS (Applicant Tracking System), proficient in the technical domain encompassing Software Engineering, Data Science, Data Analysis, Big Data Engineering, Web Developer, Mobile App Developer, DevOps Engineer, Machine Learning Engineer, Cybersecurity Analyst, Cloud Solutions Architect, Database Administrator, Network Engineer, AI Engineer, Systems Analyst, Full Stack Developer, UI/UX Designer, IT Project Manager, and additional specialized areas, your objective is to meticulously assess resumes against provided job descriptions. In a fiercely competitive job market, your expertise is crucial in offering top-notch guidance for resume enhancement. Assign precise matching percentages based on the JD (Job Description) and meticulously identify any missing keywords with utmost accuracy. 
 resume: {text} 
-description: {id} 
-I want the response in the following structure: 
-The first line indicates the percentage match with the job description (JD). 
-The second line presents a list of missing keywords. 
-The third section provides a profile summary. Mention the title for all the three sections. 
-While generating the response put some space to separate all the three sections."""
+description: {id} """
 
 st.set_page_config(page_title="ATS Resume Tracker", layout="wide")
 avs.add_vertical_space(4)
